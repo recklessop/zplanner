d5756b3c920ed12d8682e8e0ad2b9431789fa125
Justin Paul <justin@jpaul.me>
2019-03-01 13:17:43 -0500

    Create config-vm.ps1

---
+++ ova/config-vm.ps1
@@ -0,0 +1,5 @@
+$vm_ip = $ENV:vm_ip
+Write-Output $vm_ip
+
+ssh -i .ssh\id_rsa.pub zplanner@$vm_ip "sudo apt update; sudo apt upgrade -y;"
+ssh -i .ssh\id_rsa.pub zplanner@$vm_ip  "git clone https://github.com/recklessop/unifi-va.git; cd unifi-va; sudo bash /home/unifi/unifi-va/install.sh"
